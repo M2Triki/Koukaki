a363f5cbf2a013d35afe4ce86a71747aafd668c2
Matheo TRIKI <matheo.triki@gmail.com>
2024-07-25 17:35:05 +0200

    Création fichier scriptjs et enqueue style du stylecss enfant et scripjs enfant dans functionsphp

--- app/public/wp-content/themes/foce-child/functions.php
+++ app/public/wp-content/themes/foce-child/functions.php
@@ -2,6 +2,8 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+    wp_enqueue_script( 'child-script', get_stylesheet_directory_uri() . '/js/script.js', array(), 0.1, true );
 }
 
 // Get customizer options form parent theme
