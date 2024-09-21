35999800576593ac8e0290e27f6215df78b00f61
Matheo TRIKI <matheo.triki@gmail.com>
2024-09-21 20:54:03 +0200

    Ajout image oscars koukaki, début création section

--- app/public/wp-content/themes/foce-child/front-page.php
+++ app/public/wp-content/themes/foce-child/front-page.php
@@ -41,6 +41,9 @@ get_header();
             </div>
         </section>
     </main>
+
+    <?php get_template_part( 'template-parts/section-oscars' ); ?>
+
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/skrollr/0.6.30/skrollr.min.js"></script>
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script>
