f9c40cdbb8f351c3197229093166e75495b08227
Matheo TRIKI <matheo.triki@gmail.com>
2024-09-21 20:47:24 +0200

    Création section oscars

--- app/public/wp-content/themes/foce-child/front-page.php
+++ app/public/wp-content/themes/foce-child/front-page.php
@@ -25,21 +25,24 @@ get_header();
                     <h3><span class="titre titre-animation1">Le </span><span class="titre titre-animation2">Lieu</span></h3>
                 </div>
                 <div class="container-nuages">
-                    <img src="./wp-content/uploads/2024/09/nuages.png" alt="Nuages">
+                    <img src="../wp-content/uploads/2024/09/nuages.png" alt="Nuages" data-0="transform:translateX(0px);" data-500="transform:translateX(-300px);">
                 </div>
-
             </article>
+
+            
         </section>
-
-
+        
+        
         <section id="studio">
             <h2><span class="titre titre-animation1">Studio </span><span class="titre titre-animation2">Koukaki</span></h2>
             <div>
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
                 <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
             </div>
-            </section>
+        </section>
     </main>
-
-<?php
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/skrollr/0.6.30/skrollr.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script>
+    <?php
 get_footer();
--- app/public/wp-content/themes/foce-child/js/script.js
+++ app/public/wp-content/themes/foce-child/js/script.js
@@ -1,16 +1,17 @@
-window.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('.titre'); // Sélectionne toutes les sections à animer
-  
-    sections.forEach(section => {
-      const sectionPosition = section.getBoundingClientRect().top; // Position de la section par rapport au viewport
-      const screenPosition = window.innerHeight / 1.3; // Ajuste la distance de déclenchement si nécessaire
-  
-      if (sectionPosition < screenPosition) {
-        section.classList.add('section-visible'); // Ajoute la classe pour déclencher l'animation
-      }
-    });
-  });  
+// Animation des titres lors du scroll
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll(".titre"); // Sélectionne toutes les sections à animer
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var s = skrollr.init();
-});  
+  sections.forEach((section) => {
+    const sectionPosition = section.getBoundingClientRect().top; // Position de la section par rapport au viewport
+    const screenPosition = window.innerHeight / 1.3; // Ajuste la distance de déclenchement si nécessaire
+
+    if (sectionPosition < screenPosition) {
+      section.classList.add("section-visible"); // Ajoute la classe pour déclencher l'animation
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var s = skrollr.init();
+});
