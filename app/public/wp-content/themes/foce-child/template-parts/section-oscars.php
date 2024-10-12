b75b14c802e4a6862fc673897b0855e51e4129ff
Matheo TRIKI <matheo.triki@gmail.com>
2024-10-12 13:43:05 +0200

    Ajout <span> dans H3 section oscars pour animation titre

--- app/public/wp-content/themes/foce-child/template-parts/section-oscars.php
+++ app/public/wp-content/themes/foce-child/template-parts/section-oscars.php
@@ -1,7 +1,7 @@
 <section id="oscars" class="oscars">
             <div class="oscars__title titre-animation1">
             <img src="<?php echo get_theme_file_uri() . '/assets/images/nomination_oscars_bg.png'; ?>" alt="fond du titre des nominations">
-            <h3>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>
+            <h3><span class="titre titre-animation1">Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</span></h3>
             </div>
             <img class="oscars__logo" src="<?php echo get_theme_file_uri() . '/assets/images/oscars_koukaki.png'; ?>" alt="Nomination aux Oscars image">
         </section>
